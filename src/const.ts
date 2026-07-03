5c2aa1af979373e29028aae5f12f84dff5118256
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-03 10:44:48 +0900

    feat: コンポーネント作成

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
 export const lang: string = "ja";
-export const title: string = "HTML3";
-export const description: string = "HTML3演習のサンプルや課題を作成するページです";
-export const x_name: string = "ecccomp";
+export const title: string = "ポートフォリオ";
+export const description: string = "福島駿弥のポートフォリオです";
+export const x_name: string = "fksm_0614";
 export const url: string = "https://comp.ecc.ac.jp/";
