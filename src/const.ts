d7155643150870306ad572de0791cd03a8a6e4c5
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-03 10:23:18 +0900

    feat: 変数化

---
+++ src/const.ts
@@ -0,0 +1,5 @@
+export const lang: string = "ja";
+export const title: string = "HTML3";
+export const description: string = "HTML3演習のサンプルや課題を作成するページです";
+export const x_name: string = "ecccomp";
+export const url: string = "https://comp.ecc.ac.jp/";
